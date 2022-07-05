5bee2c342b5f1e4f0b64b423639a8b1322d371a0
jerrvonewing <jerrvonewing.cs@gmail.com>
2022-07-04 23:13:26 -0500

    Added placholder for keyboard keys.

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,8 +1,28 @@
 import React from 'react'
 
 function Keyboard() {
+  const line1 = ["Q","W","E","R","T","Y","U","I","O","P"]
+  const line2 = ["A","S","D","F","G","H","J","K","L"]
+  const line3 = ["Z","X","C","V","B","N","M"]
+
   return (
-    <div>Keyboard</div>
+    <div class="keyboard">
+      <div class="line1">
+        {line1.map((key) => {
+          return <div>{key}</div>;
+        })}
+      </div>
+      <div class="line2">
+      {line2.map((key) => {
+          return <div>{key}</div>;
+        })}
+      </div>
+      <div class="line3">
+      {line3.map((key) => {
+          return <div>{key}</div>;
+        })}
+      </div>
+    </div>
   )
 }
 
