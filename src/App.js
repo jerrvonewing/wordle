aac9226a2d50e5b15b5b3ce12d19258d06cc7186
jerrvonewing <jerrvonewing.cs@gmail.com>
2022-07-21 13:58:15 -0500

    Testing letter comparison and color changing for guessing word.

--- src/App.js
+++ src/App.js
@@ -12,35 +12,38 @@ function App() {
   const [board, setBoard] = useState(boardDefault);
   const [currentAttempt, setCurrentAttempt] = useState({attempt: 0, letterPosition: 0});
 
+  const correctWord = "noisy"
+  
   const pressLetter = (keyVal) => {
     if(currentAttempt.letterPosition > 4) return;
     const newBoard = [...board]
     newBoard[currentAttempt.attempt][currentAttempt.letterPosition] = keyVal
     setBoard(newBoard)
     setCurrentAttempt({...currentAttempt, letterPosition: currentAttempt.letterPosition + 1});
-}
+  }
 
-const pressEnter = () => {
-    //Validates 5 letters are present
-    if(currentAttempt.letterPosition !== 5) return;
-    setCurrentAttempt({attempt: currentAttempt.attempt + 1, letterPosition: 0});
-}
+  const pressEnter = () => {
+      //Validates 5 letters are present
+      if(currentAttempt.letterPosition !== 5) return;
+      setCurrentAttempt({attempt: currentAttempt.attempt + 1, letterPosition: 0});
+  }
 
-const pressDelete = () => {
-    if(currentAttempt.letterPosition === 0) return;
-    setCurrentAttempt({attempt: currentAttempt.attempt, letterPosition: currentAttempt.letterPosition - 1});
+  const pressDelete = () => {
+      if(currentAttempt.letterPosition === 0) return;
+      setCurrentAttempt({attempt: currentAttempt.attempt, letterPosition: currentAttempt.letterPosition - 1});
 
-    const newBoard = [...board]
-    newBoard[currentAttempt.attempt][currentAttempt.letterPosition -1] = ""
-    setBoard(newBoard);
-}
+      const newBoard = [...board]
+      newBoard[currentAttempt.attempt][currentAttempt.letterPosition -1] = ""
+      setBoard(newBoard);
+  }
+
 
   return (
     <div className="App">
       <nav>
         <h1>Wordle Unlimited</h1>
       </nav>
-      <AppContext.Provider value={{board, setBoard, currentAttempt, setCurrentAttempt,pressLetter,pressEnter,pressDelete}}>
+      <AppContext.Provider value={{board, setBoard, currentAttempt, setCurrentAttempt,pressLetter,pressEnter,pressDelete, correctWord}}>
         <div className="game">
           <Board/>
           <Keyboard/>
--- src/components/Letter.js
+++ src/components/Letter.js
@@ -2,10 +2,15 @@ import React, {useContext} from 'react'
 import { AppContext} from '../App'
 
 function Letter({letterPosition, attemptValue}) {
-    const {board,setBoard} = useContext(AppContext)
+    const {board, currentAttempt, correctWord} = useContext(AppContext)
+    console.log("The word is " + correctWord)
     const letter = board[attemptValue][letterPosition];
+
+    const correct = correctWord[letterPosition] === letter
+    const almost = !correct && letter !== "" && correctWord.includes(letter)
+    const letterState = currentAttempt.attempt > attemptValue && (correct ? "correct" : almost ? "almost" : "error")
   return (
-        <div className="letter">{letter}</div>
+        <div className="letter" id={letterState}>{letter}</div>
   )
 }
 
